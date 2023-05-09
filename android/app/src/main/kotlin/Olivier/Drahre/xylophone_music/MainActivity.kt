725a81e054ec0f6b448f47b9bcb80e032da93458
Olivier Erhard <o.erhard@gmail.com>
2023-05-08 23:19:48 -0300

    base

---
+++ android/app/src/main/kotlin/Olivier/Drahre/xylophone_music/MainActivity.kt
@@ -0,0 +1,6 @@
+package Olivier.Drahre.xylophone_music
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const xylophone());
+}
+
+class xylophone extends StatelessWidget {
+  const xylophone({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(child: Container(
+          child: ,
+        )
+
+        ),
+        ),
+    );
+  }
+}
+
